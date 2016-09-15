aae870fb1bd2addeb86b19ae1f882581dae07860
Victor <vic.uceda@estudiante.uam.es>
2016-09-15 21:22:55 +0200

    read 3 IR sensor with fast I2C

--- Firmware/Examples/leerIR/leerIR.ino
+++ Firmware/Examples/leerIR/leerIR.ino
@@ -16,20 +16,24 @@ uint8_t medida1=0;
 uint8_t IR_value1[3]={0};
 uint8_t IR_value2[3]={0};
 void loop() {
-  
-  medida1 = robot.analogRead(0); //robot.analogScand(3, IR_value1);
+  long t1= micros();
+  //medida1 = robot.analogRead(0); //
+  robot.analogScand(3, IR_value1);  
   robot.digitalWrite(0,HIGH);
   //delayMicroseconds(40);
-  medida2 =robot.analogRead(0); //robot.analogScand(3, IR_value2);
+  //medida2 =robot.analogRead(0); //
+  robot.analogScand(3, IR_value2);
   robot.digitalWrite(0,LOW);
+  long t2= micros();
   
-  //Serial.print(t2-t1);Serial.print(" , ");Serial.print(medida1);Serial.print(" ");Serial.print(medida2);Serial.print(" ");
+  Serial.print(t2-t1);Serial.print(" , ");Serial.print(medida1);Serial.print(" ");Serial.print(medida2);Serial.print(" ");
   Serial.println(medida2-medida1); 
-  /*for(int i=0;i<3;i++){
+  Serial.print("MEDIDAS: ");
+  for(int i=0;i<3;i++){
     Serial.print(IR_value2[i] - IR_value1[i]);
     Serial.print(" ");
   }
-  Serial.println("");*/
+  Serial.println("");
 
   delay(100);
 
